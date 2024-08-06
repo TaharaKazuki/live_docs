c87d1859799e597d7ac1ad4c8d7a5d7cd21c011a
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-08-06 10:34:37 +0900

    update

---
+++ components/CollaborativeRoom.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { SignedOut, SignInButton, SignedIn, UserButton } from '@clerk/nextjs';
+import { RoomProvider, ClientSideSuspense } from '@liveblocks/react/suspense';
+import React from 'react';
+
+import { Editor } from './editor/Editor';
+import Header from './Header';
+
+const CollaborativeRoom = () => {
+  return (
+    <RoomProvider id="my-room">
+      <ClientSideSuspense fallback={<div>Loading...</div>}>
+        <Header>
+          <div className="flex w-fit items-center justify-center gap-2">
+            <p className="document-title">Share</p>
+          </div>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </Header>
+        <Editor />
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
+
+export default CollaborativeRoom;
