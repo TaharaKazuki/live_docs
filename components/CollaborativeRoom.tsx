f4b00417b5cf480a9f8c43bad5160cedcca83bf1
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-08-08 13:19:47 +0900

    update

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -2,30 +2,82 @@
 
 import { SignedOut, SignInButton, SignedIn, UserButton } from '@clerk/nextjs';
 import { RoomProvider, ClientSideSuspense } from '@liveblocks/react/suspense';
-import React from 'react';
+import Image from 'next/image';
+import React, { useRef, useState } from 'react';
 
 import { Editor } from './editor/Editor';
 import Header from './Header';
+import { Input } from '@/components/ui/input';
 
 const CollaborativeRoom = ({
   roomId,
   roomMetadata,
 }: CollaborativeRoomProps) => {
+  console.info('roomemeta', roomMetadata);
+  const [documentTitle, setDocumentTitle] = useState<string>(
+    roomMetadata.title
+  );
+  const [editing, setEditing] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLDivElement>(null);
+
+  const currentUserType = 'editor';
+  const updateTitleHandler = () => {};
+
   return (
-    <RoomProvider id="my-room">
+    <RoomProvider id={roomId}>
       <ClientSideSuspense fallback={<div>Loading...</div>}>
-        <Header>
-          <div className="flex w-fit items-center justify-center gap-2">
-            <p className="document-title">Share</p>
-          </div>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </Header>
-        <Editor />
+        <div className="collaborative-room">
+          <Header>
+            <div
+              ref={containerRef}
+              className="flex w-fit items-center justify-center gap-2"
+            >
+              {editing && !loading ? (
+                <Input
+                  type="text"
+                  value={documentTitle}
+                  ref={inputRef}
+                  placeholder="Enter title"
+                  onChange={(e) => setDocumentTitle(e.target.value)}
+                  onKeyDown={updateTitleHandler}
+                  disable={!editing}
+                  className="document-title-input"
+                />
+              ) : (
+                <>
+                  <p className="document-title">{documentTitle}</p>
+                </>
+              )}
+
+              {currentUserType === 'editor' && !editing && (
+                <Image
+                  src="/assets/icons/edit.svg"
+                  alt="edit"
+                  width={24}
+                  height={24}
+                  onClick={() => setEditing(true)}
+                  className="cursor-pointer"
+                />
+              )}
+
+              {currentUserType !== 'editor' && !editing && (
+                <p className="view-only-tag">View only</p>
+              )}
+
+              {loading && <p className="text-sm text-gray-400">saving...</p>}
+            </div>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </Header>
+          <Editor />
+        </div>
       </ClientSideSuspense>
     </RoomProvider>
   );
